c5e41b284654de0398d48bb133220689d7e0d436
MrDotGamer <40805382+MrDotGamer@users.noreply.github.com>
2021-03-22 21:38:13 +0200

    Add project files.

---
+++ Calc/Loan.cs
@@ -0,0 +1,157 @@
+﻿using CommandLine;
+using System;
+using System.IO;
+
+namespace Calc
+{
+    public enum LoanData
+    {
+        LOANCODE,
+        STANDARTCONTRIBUTION,
+        LOANSTARTDATE,
+        LOANENDDATE,
+        LATESTCONTRIBUTIONPAIDVALUE,
+        DATEOFLASTPAYMENT
+    }
+    public class Loan
+    {
+        private string _loanCode { get; set; }
+        private decimal _standartContribution { get; set; }
+        private DateTime _loanStartDate { get; set; }
+        private DateTime _loanEndDate { get; set; }
+        private decimal _latestContributionsPaidValue { get; set; }
+        private DateTime _dateOfLastPayment { get; set; }
+        private readonly IDiscountCounter _discountCounter;
+        public Loan(string[] clientLoansData,DateTime inspectionDate)
+        {
+            for (int i = 0; i < clientLoansData.Length - 1; i++)
+            {
+                switch (i)
+                {
+                    case (int)LoanData.LOANCODE:
+                        _loanCode = clientLoansData[i];
+                        break;
+                    case (int)LoanData.STANDARTCONTRIBUTION:
+                        _standartContribution = decimal.Parse(clientLoansData[i]);
+                        break;
+                    case (int)LoanData.LOANSTARTDATE:
+                        _loanStartDate = DateTime.Parse(clientLoansData[i]);
+                        break;
+                    case (int)LoanData.LOANENDDATE:
+                        _loanEndDate = DateTime.Parse(clientLoansData[i]);
+                        break;
+                    case (int)LoanData.LATESTCONTRIBUTIONPAIDVALUE:
+                        _latestContributionsPaidValue = decimal.Parse(clientLoansData[i]);
+                        break;
+                    case (int)LoanData.DATEOFLASTPAYMENT:
+                        _dateOfLastPayment = DateTime.Parse(clientLoansData[i]);
+                        break;
+                }
+            }
+            _discountCounter = new DiscountCounter(_loanStartDate,_standartContribution,_latestContributionsPaidValue,_loanCode,inspectionDate);
+        }
+        public DiscountForCode GetNextMonthPayment()
+        {
+            return _discountCounter.ReturnPaymentWithBiggestDiscount();
+        }
+    }
+    public class DiscountForCode
+    {
+        private readonly string _loanCode;
+        private readonly decimal _contributionWithDiscount;
+        public DiscountForCode(string loanCode,decimal contributionWithDiscount)
+        {
+            _loanCode = loanCode;
+            _contributionWithDiscount = contributionWithDiscount;
+        }
+        public string LoanCode()
+        {
+            return _loanCode;
+        }
+        public decimal StandartContributionWithDiscount()
+        {
+            return _contributionWithDiscount;
+        }
+    }
+    public class DiscountCounter : IDiscountCounter
+    {
+        private readonly DateTime _loanStartDate;
+        private readonly decimal _standartContribution;
+        private readonly decimal _latestContributionsPaidValue;
+        private readonly string _loanCode;
+        private readonly DateTime _inspectionDate;
+        public DiscountCounter(DateTime loanStartDate, decimal standartContribution, decimal latestContributionsPaidValue,string loanCode,DateTime inspectionDate)
+        {
+            _loanStartDate = loanStartDate;
+            _latestContributionsPaidValue = latestContributionsPaidValue;
+            _standartContribution = standartContribution;
+            _loanCode = loanCode;
+            _inspectionDate = inspectionDate;
+        }
+        private bool OnePercentDiscount()
+        {
+            var i = Math.Abs(((_loanStartDate.Year - _inspectionDate.Year) * 12) + _loanStartDate.Month - _inspectionDate.Month);
+            if (i % 10 == 0)
+                return true;
+            return false;
+        }
+        private bool EvenNumberDiscont()
+        {
+            var i = Math.Abs(((_loanStartDate.Year - _inspectionDate.Year) * 12) + _loanStartDate.Month - _inspectionDate.Month);
+            if (_standartContribution % 2 == 0 && i % 18 == 0)
+                return true;
+            return false;
+        }
+        private bool DoublePremiumDiscount()
+        {
+            if ((_latestContributionsPaidValue > (_standartContribution * 2)) && (_standartContribution > 100))
+                return true;
+            return false;
+        }
+        public DiscountForCode ReturnPaymentWithBiggestDiscount()
+        {
+            decimal minPayment = _standartContribution; ;
+            decimal contribution = _standartContribution;
+            if (OnePercentDiscount())
+            {
+                contribution = _standartContribution - (_standartContribution * 1 / 100);
+                if (Decimal.Compare(contribution, minPayment) == -1)
+                {
+                    minPayment = contribution;
+                }
+            }
+            if (DoublePremiumDiscount())
+                contribution = _standartContribution - (_standartContribution % 100);
+                if (Decimal.Compare(contribution, minPayment) == -1)
+                {
+                    minPayment = contribution;
+                }
+            if (EvenNumberDiscont())
+                minPayment = 0;
+            return new DiscountForCode(_loanCode, minPayment);
+        }
+    }
+    public interface IDiscountCounter
+    {
+        DiscountForCode ReturnPaymentWithBiggestDiscount();
+    }
+    public class Arguments
+    {
+        [Option('r', "read", Required = true, HelpText = "Input file to be processed.")]
+        public string InputFrom { get; set; }
+        [Option('w', "write", Required = false, HelpText = "output file to be processed.")]
+        public string InputTo { get; set; }
+        [Option('t', "date", Required = false, HelpText = "inspection date (Y,M,D)")]
+        public string InspectionDate { get; set; }
+       public string FileName
+        {
+            get
+            {
+                var uri = new Uri(InputFrom);
+                string filename = Path.GetFileName(uri.LocalPath);
+                var index = filename.IndexOf(".");
+                return filename.Remove(index); ;
+            }
+        }
+    }
+}
---
+++ Calc/Program.cs
@@ -0,0 +1,70 @@
+﻿using CommandLine;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Arguments arguments = new Arguments();
+            Parser.Default.ParseArguments<Arguments>(args).WithParsed<Arguments>(a => 
+            {
+                arguments.InputFrom = a.InputFrom;
+                if (a.InputTo == null)
+                {
+                    var index = a.InputFrom.LastIndexOf("\\");
+                    arguments.InputTo = a.InputFrom.Remove(index) + $"\\{a.FileName}_resultatai.csv";
+                }
+                else
+                {
+                    arguments.InputTo = a.InputTo + $"\\{a.FileName}_resultatai.csv";
+                }
+                if (a.InspectionDate == null)
+                {
+                    arguments.InspectionDate = "2021,1,2";
+                }
+                else
+                {
+                    arguments.InspectionDate = a.InspectionDate;
+                }
+            });
+            try
+            {
+                await ReadAndWriteContributions(arguments.InputFrom, arguments.InputTo, DateTime.Parse(arguments.InspectionDate));
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("An error occured: " + ex.Message);
+            }
+        }
+        private static async Task ReadAndWriteContributions(string from, string to,DateTime inspectionDate)
+        {
+            using FileStream inputStream = new FileStream(from, FileMode.Open, FileAccess.Read);
+            using var sr = new StreamReader(inputStream, Encoding.UTF8);
+            using FileStream outputStream = new FileStream(to, FileMode.OpenOrCreate);
+            using var sw = new StreamWriter(outputStream, Encoding.UTF8);
+            string line;
+            while ((line = await sr.ReadLineAsync()) != null)
+            {
+                var client = GetCodeWithDiscount(line,inspectionDate);
+                await sw.WriteAsync(client);
+                outputStream.Flush();
+            }
+        }
+        private static string GetCodeWithDiscount(string line,DateTime inspectionDate)
+        {
+            Loan loan = new Loan(line.Split(';'), inspectionDate);
+            var codeAndDiscount = loan.GetNextMonthPayment();
+            string discount = codeAndDiscount.StandartContributionWithDiscount().ToString();
+            if (codeAndDiscount.StandartContributionWithDiscount() == 0)
+            {
+                discount = "-";
+            }
+            return $"{codeAndDiscount.LoanCode()};{discount};\n";
+        }
+    }
+}
